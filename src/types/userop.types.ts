dc0470e0cfc1e746350522938e74fd0850fea5a7
Kanishka <kanishkatn@gmail.com>
2024-05-10 18:10:00 +0530

    add userop type

---
+++ src/types/userop.types.ts
@@ -0,0 +1,18 @@
+/**
+ * User Operation
+ * 
+ * Represents the structure of a user operation.
+ */
+export type UserOperation = {
+    sender: string;
+    nonce: bigint;
+    initCode: string;
+    callData: string;
+    callGasLimit: bigint;
+    verificationGasLimit: bigint;
+    preVerificationGas: bigint;
+    maxFeePerGas: bigint;
+    maxPriorityFeePerGas: bigint;
+    paymasterAndData: string;
+    signature: string;
+}
